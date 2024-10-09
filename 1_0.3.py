d32a3d0d95b2504385992cbc2a6e924ae4837d63
Grizzlazy <121704215+Hoanghuy1402@users.noreply.github.com>
2024-10-09 22:43:58 +0700

    Test_N

--- 1_0.3.py
+++ 1_0.3.py
@@ -33,10 +33,10 @@ epsilon = (-1) * 0.00001
 LOOP_IMPROVED = 0
 SET_LAST_10 = [] 
 BEST = []
-number_of_cities = int(os.getenv('NUMBER_OF_CITIES', '10')) 
-delta = float(os.getenv('DELTA', '0.3'))
-alpha = json.loads(os.getenv('ALPHA', '[0.5, 0.3, 0.1]'))
-END_SEGMENT = int(os.getenv('END_SEGMENT', '100'))
+number_of_cities = int(sys.argv[1])
+delta = float(sys.argv[2])
+alpha = json.loads(sys.argv[3])
+END_SEGMENT = int(sys.argv[4])
 
 SEGMENT = 100
 solution_pack_len = 0
@@ -416,25 +416,63 @@ txt_files = glob.glob(os.path.join(folder_path, "C201_0.5.dat"))
             # glob.glob(os.path.join(folder_path, "*3.dat"))
 # txt_files = ["test_data\\Smith\\TSPrd(time)\\Solomon\\15\\RC101_1.dat", "test_data\\Smith\\TSPrd(time)\\Solomon\\15\\RC101_2.5.dat", "test_data\\Smith\\TSPrd(time)\\Solomon\\15\\RC101_2.dat", "test_data\\Smith\\TSPrd(time)\\Solomon\\15\\RC101_3.dat"]
 # Tạo một tệp Excel mới
-# 
+workbook = openpyxl.Workbook()
+sheet = workbook.active
 
-output_file_path = f"Random_{number_of_cities}_{delta}_{alpha}_{END_SEGMENT}_CL1.txt"
-with open(output_file_path, "w") as output_file:
-    output_file.write("File, Iteration, Best Fitness, Run Time, Best Solution\n")
+# Dòng và cột bắt đầu ghi kết quả
+row = 1
+column = 1
 
+# Ghi tên file .txt vào cột đầu tiên
 for txt_file in txt_files:
+    sheet.cell(row=row, column=column, value=os.path.basename(txt_file))
+    row += 1
+# Đặt lại dòng và cột cho việc ghi kết quả
+row = 1
+for txt_file in txt_files:
+    column = 2
     with open(txt_file, 'r') as file:
+        # Đọc nội dung từ file .txt và xử lý nó
+        # print(txt_file)
+        # log = os.path.basename(txt_file)+ f'{Data.number_of_cities}_{delta}_{alpha1}_{END_SEGMENT}_CL1_log_new.log'
+        # log_folder = 'Result\log_result'
+        # log_file_path = os.path.join(log_folder, log)
+        # log_file = open(log_file_path, 'w')
+        # sys.stdout = log_file
         Data.read_data_random(txt_file)
+        result = []
+        run_time = []
+        avg = 0
+        avg_run_time = 0
+        best_csv_fitness = 1000000
         for i in range(ITE):
             BEST = []
             print("------------------------",i,"------------------------")
             start_time = time.time()
             best_fitness, best_sol = Tabu_search_for_CVRP(1)
-            end_time = time.time()
-            run_time = end_time - start_time
             print("---------- RESULT ----------")
             print(best_sol)
             print(best_fitness)
-            # Append results to the text file for each iteration
-            with open(output_file_path, "a") as output_file:
-                output_file.write(f"{os.path.basename(txt_file)}, {i}, {best_fitness}, {run_time}, {best_sol}\n")
+            avg += best_fitness/ITE
+            result.append(best_fitness)
+            # print(Function.Check_if_feasible(best_sol))
+            end_time = time.time()
+            run = end_time - start_time
+            run_time.append(run)
+            avg_run_time += run/ITE
+            sheet.cell(row=row, column=column, value=best_fitness)
+
+            column += 1
+            if best_csv_fitness > best_fitness:
+                best_csv_sol = best_sol
+                best_csv_fitness = best_fitness
+            if i == ITE - 1:
+                sheet.cell(row=row, column=column, value=avg_run_time)
+                sheet.cell(row=row, column=column+1, value=str(best_csv_sol))    
+        # Tăng dòng cho lần chạy tiếp theo
+        row += 1
+    workbook.save(f"Random_{number_of_cities}_{delta}_{alpha}_{END_SEGMENT}_CL1.xlsx")
+        # log_file.close()
+
+workbook.close()
+
