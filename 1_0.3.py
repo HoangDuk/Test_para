84e0b1bfeb1cdd55e3c28c72236ee16c40df4481
Grizzlazy <cuong.pt311@gmail.com>
2024-10-09 14:17:07 +0700

    Update

--- 1_0.3.py
+++ 1_0.3.py
@@ -14,6 +14,7 @@ import csv
 import numpy as np
 import math
 import sys
+import json
 global LOOP
 global tabu_tenure
 global best_sol
@@ -32,14 +33,12 @@ epsilon = (-1) * 0.00001
 LOOP_IMPROVED = 0
 SET_LAST_10 = [] 
 BEST = []
-# 
-delta = 0.3
-alpha1 = 0.5
-alpha2 = 0.3
-alpha3 = 0.1
-SEGMENT = 100
-END_SEGMENT = 100 #50 100 150
+number_of_cities = int(os.getenv('NUMBER_OF_CITIES', '30')) 
+delta = float(os.getenv('DELTA', '0.3'))
+alpha = json.loads(os.getenv('ALPHA', '[0.5, 0.3, 0.1]'))
+END_SEGMENT = int(os.getenv('END_SEGMENT', '100'))
 
+SEGMENT = 100
 def roulette_wheel_selection(population, fitness_scores):
     total_fitness = sum(fitness_scores)
     probabilities = [score / total_fitness for score in fitness_scores]
@@ -312,11 +311,11 @@ def Tabu_search(init_solution, tabu_tenure, CC, first_time, Data1, index_conside
 
             used[choose] += 1
             if flag == True:
-                score[choose] += alpha1
+                score[choose] += alpha[0]
             elif current_fitness - prev_fitness < epsilon:
-                score[choose] += alpha2
+                score[choose] += alpha[1]
             else:
-                score[choose] += alpha3
+                score[choose] += alpha[2]
 
             for j in range(len(nei_set)):
                 if used[j] == 0:
@@ -407,7 +406,7 @@ def Tabu_search_for_CVRP(CC):
     return best_fitness, best_sol
 
 # Thư mục chứa các file .txt
-folder_path = "test_data\\data_demand_random\\10\\"
+folder_path = "test_data\\data_demand_random\\"+str(number_of_cities)
 # folder_path = "test_data\\Smith\\TSPrd(time)\\Solomon\\50\\0_5TSP_50"
 # folder_path = "test_data\\Smith\\TSPrd(time)\\Solomon\\15"
 
@@ -435,11 +434,11 @@ for txt_file in txt_files:
     with open(txt_file, 'r') as file:
         # Đọc nội dung từ file .txt và xử lý nó
         # print(txt_file)
-        log = os.path.basename(txt_file)+ f'{Data.number_of_cities}_{delta}_{alpha1}_{END_SEGMENT}_CL1_log_new.log'
-        log_folder = 'Result\log_result'
-        log_file_path = os.path.join(log_folder, log)
-        log_file = open(log_file_path, 'w')
-        sys.stdout = log_file
+        # log = os.path.basename(txt_file)+ f'{Data.number_of_cities}_{delta}_{alpha1}_{END_SEGMENT}_CL1_log_new.log'
+        # log_folder = 'Result\log_result'
+        # log_file_path = os.path.join(log_folder, log)
+        # log_file = open(log_file_path, 'w')
+        # sys.stdout = log_file
         Data.read_data_random(txt_file)
         result = []
         run_time = []
@@ -473,7 +472,7 @@ for txt_file in txt_files:
             workbook.save(f"Result\excel_result\Random_{Data.number_of_cities}_{delta}_{alpha1}_{END_SEGMENT}_CL1.xlsx")
         # Tăng dòng cho lần chạy tiếp theo
         row += 1
-        log_file.close()
+        # log_file.close()
 
 workbook.close()
 
